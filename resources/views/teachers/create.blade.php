9335aed0bf9ddcb0372b270504c4e8137825fdb9
shafa <132644198+shafakirrn@users.noreply.github.com>
2024-04-16 11:43:53 +0700

    update create and edit teacher

--- resources/views/teachers/create.blade.php
+++ resources/views/teachers/create.blade.php
@@ -1,36 +1,32 @@
 @extends('layouts.app')
   
-@section('title', 'Edit Teacher')
+@section('title', 'Create Teacher')
   
 @section('contents')
-    <h1 class="mb-0">Edit Teacher</h1>
+    <h1 class="mb-0">Add Teacher</h1>
     <hr />
-    <form action="{{ route('teachers.update', $teacher->id) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('teachers.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        @method('PUT')
-        <div class="row">
-            <div class="col mb-3">
-                <label class="form-label">Name</label>
-                <input type="text" name="name" class="form-control" placeholder="Name" value="{{ $teacher->name }}" >
+        <div class="row mb-3">
+            <div class="col">
+                <input type="text" name="name" class="form-control" placeholder="Name">
             </div>
         </div>
-        <div class="row">
-            <div class="col mb-3">
-                <label class="form-label">Address</label>
-                <input type="text" name="address" class="form-control" placeholder="Address" value="{{ $teacher->address }}" >
+        <div class="row mb-3">
+            <div class="col">
+                <input type="text" name="address" class="form-control" placeholder="Address">
             </div>
-            <div class="col mb-3">
-                <label class="form-label">Mobile</label>
-                <input type="text" name="mobile" class="form-control" placeholder="Mobile" value="{{ $teacher->mobile }}" >
+            <div class="col">
+                <input type="text" name="mobile" class="form-control" placeholder="Mobile">
             </div>
-            <div class="col mb-3">
-                <label class="form-label">Subject</label>
-                <input type="text" name="subject" class="form-control" placeholder="Subject" value="{{ $teacher->subject }}" >
+            <div class="col">
+                <input type="text" name="subject" class="form-control" placeholder="Subject">
             </div>
         </div>
+ 
         <div class="row">
             <div class="d-grid">
-                <button class="btn btn-warning">Update</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
     </form>
---
+++ resources/views/teachers/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+  
+@section('title', 'Edit Teacher')
+  
+@section('contents')
+    <h1 class="mb-0">Edit Teacher</h1>
+    <hr />
+    <form action="{{ route('teachers.update', $teacher->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="row">
+            <div class="col mb-3">
+                <label class="form-label">Name</label>
+                <input type="text" name="name" class="form-control" placeholder="Name" value="{{ $teacher->name }}" >
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label class="form-label">Address</label>
+                <input type="text" name="address" class="form-control" placeholder="Address" value="{{ $teacher->address }}" >
+            </div>
+            <div class="col mb-3">
+                <label class="form-label">Mobile</label>
+                <input type="text" name="mobile" class="form-control" placeholder="Mobile" value="{{ $teacher->mobile }}" >
+            </div>
+            <div class="col mb-3">
+                <label class="form-label">Subject</label>
+                <input type="text" name="subject" class="form-control" placeholder="Subject" value="{{ $teacher->subject }}" >
+            </div>
+        </div>
+        <div class="row">
+            <div class="d-grid">
+                <button class="btn btn-warning">Update</button>
+            </div>
+        </div>
+    </form>
+@endsection
