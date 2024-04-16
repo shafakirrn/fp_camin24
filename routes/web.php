98060de34475f4a5225273fda6f4f21db9c01553
shafa <132644198+shafakirrn@users.noreply.github.com>
2024-04-16 15:43:18 +0700

    update course

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
